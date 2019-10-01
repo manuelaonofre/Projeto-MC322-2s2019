edbc9121c9899953aeff607f55fc3b44b6af557d
ra183020 <ra183020@felix.lab.ic.unicamp.br>
2019-10-01 17:03:21 -0300

    Sala de cinema

--- src/simuladorCompraIngresso/SalaDeCinema.java
+++ src/simuladorCompraIngresso/SalaDeCinema.java
@@ -1,5 +1,10 @@
 package simuladorCompraIngresso;
 
 public class SalaDeCinema {
-
+	private int capacidadeMax;
+	private int poltronasDisponiveis;
+	
+	public void getPoltronas() {
+		return this.poltronasDisponiveis;
+	}
 }
